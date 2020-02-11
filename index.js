9f08eb088fbc53e030731cda2d6389b356bac22b
Jelmerovereem <jelmer_overeem@hotmail.com>
2020-02-11 13:11:58 +0100

    Module lodash geinstalleerd

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Pizza is lekker');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello pizza'));
+
+app.listen(port, () => console.log('Voorbeeld app luisterend naar port ${port}!'));
