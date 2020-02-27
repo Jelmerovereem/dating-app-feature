2ab5e7bb8c67d32f66ef256751931341742a886b
Jelmerovereem <jelmer_overeem@hotmail.com>
2020-02-27 12:49:07 +0100

    cleaned up server file, wrote different approach to creating server

--- server.js
+++ server.js
@@ -1,42 +1,25 @@
-
-//Express server v1
-/*const express = require('express');
+//Express server Back-End
+const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = 8000;
 
-app.get('/', (req, res) => res.send('Hello pizza'));
+/*express.static('/static', [options]);*/
+app.use(express.static('static'));
+app.get('/', function (req, res) {
+  res.sendFile('/static/index.html')
+});
 
-app.listen(port, () => console.log('Voorbeeld app luisterend naar port ${port}!'));*/
+app.get('/about', function (req, res) {
+  console.log(req.query)
+  res.sendFile(__dirname +'/static/about.html')
+});
 
+app.get('/*', function (req, res) {
+  res.sendFile(__dirname +'/static/404.html')
+});
 
+app.get('/:userId', function (req, res) {
+  res.send(req.params.userId)
+});
 
-//Express server v2
-const express = require('express')
-
-express()
-.get('/', onhome)
-.listen(1900)
-
-function onhome(req, res) {
-	res.send('<h1>Hello client</h1>\n')
-}
-
-app.use('/static', express.static('public'))
-
-
-
-//Http server
-/*var http = require('http');
-
-http.createServer(onrequest).listen(8000);
-
-function onrequest(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  if (req.url === '/about') {
-  	res.end('This is my website!\n');
-  } else {
-  	res.end('Hello World!\n');
-  }
-  res.end('Hello World!\n');
-}*/
+app.listen(8000, () => console.log('Done... Server is running'))
