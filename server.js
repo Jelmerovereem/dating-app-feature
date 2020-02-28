96b0faa8beccf52343b50d1cdd92f0607955f6fd
Jelmerovereem <jelmer_overeem@hotmail.com>
2020-02-28 16:32:55 +0100

    Upload with gitignore

--- server.js
+++ server.js
@@ -2,14 +2,29 @@
 const express = require('express');
 const app = express();
 const PORT = 8000;
+/*
+const data = {
+  userQuery: req.params.userQuery
+}*/
 
-/*express.static('/static', [options]);*/
 app.use(express.static('static'));
-app.get('/', function (req, res) {
-  res.sendFile('/static/index.html')
+app.set('view engine', 'ejs');
+
+
+app.get('/:userQuery', function (req,res) {
+  res.render('index.ejs', {data : {userQuery: req.params.userQuery,
+                                    searchResults: ['user1', 'user2', 'user3'],
+                                    loggedIn: false,
+                                    username: 'John Lopez'}})
 });
 
-app.get('/about', function (req, res) {
+
+
+/*app.get('/', function (req, res) {
+  res.sendFile('/static/index.html')
+});*/
+
+/*app.get('/about', function (req, res) {
   console.log(req.query)
   res.sendFile(__dirname +'/static/about.html')
 });
@@ -21,5 +36,7 @@ app.get('/*', function (req, res) {
 app.get('/:userId', function (req, res) {
   res.send(req.params.userId)
 });
+*/
+
 
 app.listen(8000, () => console.log('Done... Server is running'))
