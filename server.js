18d2f3d56406949dd4224033be0ef9cfde4c9dc4
Jelmerovereem <jelmer_overeem@hotmail.com>
2020-03-17 19:56:32 +0100

    Added connection to movie database API and now the user can add movies to their profile. The added movie goes through the movie database API and checks the original title of the movie. The user has a 'favorite movies' array in the database, this array shows on their profile page. Added some comments and confirmation code too.

--- server.js
+++ server.js
@@ -1,80 +1,184 @@
 //Express server Back-End
+
+
+//require .env file so I can access secure data
+require('dotenv').config();
+
+//Require express npm package
 const express = require('express');
+
+//require mongodb for database
 const mongo = require('mongodb');
+
+//require slug package for converting every input into a string. (so users can't post javascript code into a form)
 const slug = require('slug');
+
+//require bodyparser package for getting/requesting info out of a form. (req, res)
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({
   extended: true
 });
+
+//This is the baseURl for accessing the API database
+let baseURL = 'https://api.themoviedb.org/3/';
+
+//The id of a movie in the API database
+let movieIdApi = 'null';
+
+//The APIKEY 
+let APIKEY = process.env.APIKEY;
+
+//require request package, for handling the API
+var request = require('request');
+
+//use express() with 'app'
 const app = express();
+
+//app running on port 8000
 const PORT = 8000;
-const data = [];
 
-//require .env file zodat je daar database connectie links uit kan halen
-require('dotenv').config();
+//declaring empty array variables
+let data = [];
+let dataApi = [];
+let dataZelf = [];
 
+//Middleware set-up
 app.use(express.static('static'));
 app.use(bodyParser.urlencoded({ extended: true}));
 app.set('view engine', 'ejs');
 app.post('/succes.ejs', urlencodedParser, addMovie);
+app.post('/film.ejs', urlencodedParser, zoekMovie)
 
-// Maak variable db(database) aan en zet deze eerst op null
+// Declare variable db(database) andd assign null
 let db = null
-//Dit is je database link van mongodb, dit moet goed in je .env staan
+//The url/link to database. This is reffered to the .env file (secure data)
 const url = process.env.DB_HOST + ':' + process.env.DB_PORT;
 
-
-
+//Create a connection with the mongo database
 mongo.MongoClient.connect(url, function (err, client, req, res) {
     if (err) {
+      //if there is an error, log:
       console.log('Error, database connection failed');
     } else {
+      //if the connection succeeded, log:
       console.log('database connection succeeded')
     }
-    //Zoek in de client naar de database.
+    //looking up the database in mongo.
     db = client.db(process.env.DB_NAME);
   });
 
 function addMovie(req, res) {
-    //Voeg objecten toe van 'data' array aan database
-    db.collection('dating-app').insertOne({
-      
-      title: req.body.title,
-      genre: req.body.genre,
-      description: req.body.description
+    //the movie the user adds to his profile
+    let insertedMovie = req.body.movieTitle;
+    //log this movie for confirmation
+    console.log('Movie title input: ', insertedMovie);
+
+    //search in api for the inserted movie
+    request(baseURL + 'search/movie/?api_key=' + APIKEY + '&query=' + insertedMovie, function (error, response,body) {
+      body = JSON.parse(body); //parse the outcome to object, so requesting data is possible
+      console.log('Movie title from api: ', body.results[0].original_title); //confirming the title
+
+    //Add the title of the movie into the 'favoMovies' array in the database
+    db.collection('users').updateOne({id: 1}, { $addToSet: {favoMovies: body.results[0].original_title}}, function(err, req, res) {
+      if (err) {
+        console.log('Error, could not update');
+      } else {
+        console.log('Update confirmed');
+      };
+    //Closes the function that is in updateOne()
     });
+
+    //Create a object in the database and add strings into the object. If the user adds a movie it goes into the database collection 'dating-app'
+    db.collection('addedMovies').insertOne({
+      title: req.body.movieTitle, //get title
+      stars: req.body.stars //get stars
+    });
+
+    //Push the inserted data into the data variable, so it can be rendered to succes.ejs
+    data.push({
+      title: body.results[0].original_title,
+      stars: req.body.stars
+    });
+    //Render the inserted data to the succes.ejs page
     res.render('succes.ejs', {
       data: req.body
     });
-    console.log(data);
+    //Closes the function that is in request()
+    });
+    
+    //Confirmation for the data that is added to the database
+    console.log('This data is added to the database:', req.body);
+    console.log('title: ', req.body.movieTitle);
+    console.log('stars: ', req.body.stars)
 };
 
-/*function postFormulier(req, res){
-  data.push({
-    id: ,
-    title: req.body.title,
-    genre: req.body.genre,
-    description: req.body.description
-    });
-  res.render('succes.ejs', {
-    data: req.body 
-  });
-  console.log(data);
-  console.log(req.body);
-};*/
+//This function is to search for movies in the API database, this can be ignored! Has nothing to do with the dating-app feature.
+function zoekMovie(req, res) {
+  let zoekveld = req.body.zoekveld;
+  console.log('Searched on: ', zoekveld);
 
-app.get('/', function (req, res){
-  res.render('profile.ejs')
+  request(baseURL + 'search/movie/?api_key=' + APIKEY + '&query=' + zoekveld, function (error, response, body) {
+    body = JSON.parse(body);
+/*    console.log('error:', error);
+    console.log('statusCode:', response); 
+    console.log('body:', body);*/
+    /*console.log('Filmtitel:', body.results[0].original_title);
+    console.log('Beschrijving:', body.results[0].overview);*/
+    /*dataApi.push({
+      title: body.results[0].original_title,
+      description: body.results[0].overview 
+    });*//*
+    dataZelf.push({
+      lol: "test",
+      youtubeLink: 'https://www.youtube.com/results?search_query=' + body.results[0].original_title
+    })*/
+    let youtubeLink = 'https://www.youtube.com/results?search_query=' + body.results[0].original_title;
+    res.render('film.ejs', {
+      dataApi: body.results[0],
+      dataZelf: youtubeLink
+    });
+    console.log('youtubelink= ', youtubeLink);
+    console.log('dataZelf= ', dataZelf);
+    //console.log('dataZelf.youtubeLink= ', dataZelf[0].youtubeLink);
+    //console.log(req.body);
+    //console.log(body.results[0])
+    console.log(dataApi);
+    //console.log('Object:', dataApi[0]);
+    //console.log('description: ', dataApi[0].description);
+    //console.log('dit is data: ', dataApi);
+  });
+};
+
+//The homepage / profile page
+app.get('/', function (req, res) {
+  //look for the only user in database, my jobstory doesn't need more users than 1.
+  db.collection('users').findOne({
+  _id: mongo.ObjectID('5e70f258881f3e33fc62b39f')
+  }, done); 
+
+  //Check if the user has been found
+  function done(err, data) {
+  if (err) {
+    console.log('Error, cannot find the object/user name');
+  } else {
+    console.log('Found user');
+  };
+  //Confirm data from the user
+  console.log('naam= ', data.name);
+  console.log('Favomovies: ', data.favoMovies)
+
+  //Render his database / profile info into the profile page
+  res.render('profile.ejs', {
+    data: data
+  });
+};
 });
 
 app.get('/contact', function (req, res){
   res.render('contact.ejs')
 });
 
-app.get('/add', function (req, res) {
-  res.render('add.ejs')
-});
-
+//This get/function is to search for movies in the API database, this can be ignored! Has nothing to do with the dating-app feature.
 app.get('/movie', function (req, res) {
   //Find object in database collection, after that invoke/perform 'done' function
   db.collection('dating-app').findOne({
@@ -96,6 +200,8 @@ app.get('/movie', function (req, res) {
   };
 });
 
+
+//This get/function is to search for movies in the API database, this can be ignored! Has nothing to do with the dating-app feature.
 app.get('/movies', function (req, res) {
   //find all objects in database collection
   var arrayCollection = db.collection('dating-app').find().toArray(done);
@@ -113,14 +219,12 @@ app.get('/movies', function (req, res) {
   };
 });
 
-app.get('/:userQuery', function (req,res) {
+/*app.get('/:userQuery', function (req,res) {
   res.render('search.ejs', {data : {userQuery: req.params.userQuery,
                                     searchResults: ['user1', 'user2', 'user3'],
                                     loggedIn: false,
                                     username: 'John Lopez'}})
-});
-
-
+});*/
 /*app.get('/', function (req, res) {
   res.sendFile('/static/index.html')
 });*/
@@ -139,5 +243,5 @@ app.get('/:userId', function (req, res) {
 });
 */
 
-
+//Confirm server running
 app.listen(8000, () => console.log('Done... Server is running'))
