19691766952f8dfbe7c5e1598fea7b5f48be7fc7
Jelmerovereem <jelmer_overeem@hotmail.com>
2020-03-17 12:44:32 +0100

    Now you can add objects to the database through a form and the movie.ejs page can show a specific object out of the database

--- server.js
+++ server.js
@@ -1,63 +1,52 @@
 //Express server Back-End
 const express = require('express');
 const mongo = require('mongodb');
-var slug = require('slug');
-var bodyParser = require('body-parser');
+const slug = require('slug');
+const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({
   extended: true
 });
 const app = express();
 const PORT = 8000;
-const data = [{
-  id: '1',
-  title: 'Wonder woman',
-  genre: 'Action',
-  description: 'Cool DC Comics movie about a greek Goddess',
-},
-{
-  id: '2',
-  title: 'Titanic',
-  genre: 'Romance',
-  description: 'Best movie ever.',
-},
-{
-  id: '3',
-  title: 'The Expandables 1',
-  genre: 'Action',
-  description: 'Cool action movie starring Sylvester Stallone',
-},
-{
-  id: '4',
-  title: 'The Expandables 2',
-  genre: 'Action',
-  description: 'Cool action movie starring Sylvester Stallone part 2',
-}
-];
+const data = [];
 
 //require .env file zodat je daar database connectie links uit kan halen
 require('dotenv').config();
+
+app.use(express.static('static'));
+app.use(bodyParser.urlencoded({ extended: true}));
+app.set('view engine', 'ejs');
+app.post('/succes.ejs', urlencodedParser, addMovie);
 
 // Maak variable db(database) aan en zet deze eerst op null
 let db = null
 //Dit is je database link van mongodb, dit moet goed in je .env staan
 const url = process.env.DB_HOST + ':' + process.env.DB_PORT;
 
-mongo.MongoClient.connect(url, function (err, client) {
-  if (err) throw err
-  //maak eerst via compass een database aan! Zet die naam in je .env onder DB_NAME
-  db = client.db(process.env.DB_NAME);
-  //checken of je client/database correct is geconnect
-  console.log(client);
-  //Voeg objecten toe van 'data' array
-  db.collection('dating-app').insertMany(data); 
-});
 
 
-app.use(express.static('static'));
-app.use(bodyParser.urlencoded({ extended: true}));
-app.set('view engine', 'ejs');
-app.post('/succes.ejs', urlencodedParser, postFormulier);
-app.post('/contact.ejs', urlencodedParser, postFormulier)
+mongo.MongoClient.connect(url, function (err, client, req, res) {
+    if (err) {
+      console.log('Error, database connection failed');
+    } else {
+      console.log('database connection succeeded')
+    }
+    //Zoek in de client naar de database.
+    db = client.db(process.env.DB_NAME);
+  });
+
+function addMovie(req, res) {
+    //Voeg objecten toe van 'data' array aan database
+    db.collection('dating-app').insertOne({
+      title: req.body.title,
+      genre: req.body.genre,
+      description: req.body.description
+    });
+    res.render('succes.ejs', {
+      data: req.body
+    });
+    console.log(data);
+};
 
 function postFormulier(req, res){
   data.push({
@@ -70,7 +59,7 @@ function postFormulier(req, res){
   });
   console.log(data);
   console.log(req.body);
-}
+};
 
 app.get('/', function (req, res){
   res.render('profile.ejs')
@@ -82,6 +71,29 @@ app.get('/contact', function (req, res){
 
 app.get('/add', function (req, res) {
   res.render('add.ejs')
+});
+
+app.get('/movie', function (req, res) {
+  //Find object in database collection, after that invoke/perform 'done' function
+  db.collection('dating-app').findOne({
+    _id: mongo.ObjectID('5e70ae1f4b9f5d017b3e215a')
+  }, done);
+
+  //if there is an error, log the error. If there is no error log confirmation and log the object
+  function done(err, data) {
+    if (err) {
+      console.log('Error, cannot find mongo objectID');
+    } else {
+      console.log('Found mongo objectID');
+      /*console.log(data.title);*/
+    };
+    console.log(data.title);
+    //render the object to movie.ejs
+    res.render('movie.ejs', {
+      data: data
+    });
+  };
+  
 });
 
 app.get('/:userQuery', function (req,res) {
